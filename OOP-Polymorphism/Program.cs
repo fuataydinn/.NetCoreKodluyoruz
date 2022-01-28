5e6192b505875e0cfe132c8b6cb2f67eee2e5f38
Fuat Aydin <81709694+fuataydinn@users.noreply.github.com>
2022-01-28 22:14:02 +0300

    added

---
+++ OOP-Polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Polimorphism ( Çok biçimlilik ) -- Virtual anahtar kelimesi ile yapılır.
+            // Virtual anahtar kelimesi metotlarda kullanılır. Virtual anahtar kelimesi ile sanal metotlar olusturulur.
+            //Sanal metotlar : Kalıtım alınan üst sınıf içerisinde yazılan ve alt sınıflar tarafından yeniden yazılabilen metotlar.-- 
+            //Overide : Bir üst sınıfta virtual anahtar kelimesi ile işaretlemiş oldugumuz metodu biçimini degistirmek için overide kullanılır
+
+
+            //Inheritance projesine git orada uyguladık
+           // Obur taraftaki halini 1 kere çalıstır. Buna ek olarak baska metottda calıstır.
+
+
+
+
+        }
+    }
+}
